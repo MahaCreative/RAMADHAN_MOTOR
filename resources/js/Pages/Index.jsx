d87781e32ae205c7d60f81a05bd5fbb8a7adbb1b
MahaCreative <gunturmadjid.3@gmail.com>
2024-06-11 08:44:47 -0700

    a

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -7,7 +7,7 @@ import KontrolMotor from "./KontrolMotor";
 
 export default function Index(props) {
     const pengguna = props.pengguna;
-    const [menu, setMenu] = useState("");
+    const [menu, setMenu] = useState("kontrol");
     return (
         <div className="w-full h-full overflow-x-hidden">
             <div className="my-3 text-center bg-gray-800/50 bg-gradient-to-bl from-gray-700/50 via-transparent ring-1 ring-inset ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500 text-red-500 py-2 px-4 active:scale-105 cursor-pointer justify-between items-center">
